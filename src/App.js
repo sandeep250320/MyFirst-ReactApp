9c02b5ddc5404e2e5bca10dcd618d0841f05ae1a
sandeep250320 <skmayil123@gmail.com>
2021-10-16 21:58:59 +0530

    1st site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import style from './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props){
+
+    return(
+    <div className="parent">
+        <div className="child">
+            <img src={props.imgsrc} alt="img"/>
+            
+            </div>
+            <div className="internal">
+               <span className="info"> {props.info}</span>
+                <h2 className="sname">{props.sname}</h2>
+                <a href={props.link} target="_blank">
+                <button>Watch now</button></a>
+            </div>
+        
     </div>
-  );
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <>
+    <App   imgsrc="./mg.jpg"
+        info="South Indian Movie in Hindi"
+        sname="Maghadheera"
+        link="https://www.youtube.com/watch?v=js4HJnv4Zu4" 
+    />
+    <App   imgsrc="./phantom.jpg"
+        info="Bollywood movies"
+        sname="Phantom"
+        link="https://www.youtube.com/watch?v=U7vBj2AGG3c" 
+    />
+    <App   imgsrc="./mg.jpg"
+        info="South Indian Movie in Hindi"
+        sname="Maghadheera"
+        link="https://www.youtube.com/watch?v=js4HJnv4Zu4" 
+    />
+    <App   imgsrc="./phantom.jpg"
+        info="Bollywood movies"
+        sname="Phantom"
+        link="https://www.youtube.com/watch?v=U7vBj2AGG3c" 
+    />
+    </> ,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
