a7efdbc1891b80aab948277ec9c040f950d6b4f2
sandeep250320 <skmayil123@gmail.com>
2021-10-16 22:24:58 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
-import style from './App.css';
+import  './App.css';
 
 function App(props){
 
     return(
-    <div className="parent">
-        <div className="child">
-            <img src={props.imgsrc} alt="img"/>
-            
-            </div>
-            <div className="internal">
-               <span className="info"> {props.info}</span>
-                <h2 className="sname">{props.sname}</h2>
-                <a href={props.link} target="_blank">
-                <button>Watch now</button></a>
-            </div>
-        
+    
+<div className="child">
+<img src={props.imgsrc} alt="img"/>
+    
+    
+    <div className="internal">
+        <span className="info"> {props.info}</span>
+        <h2 className="sname">{props.sname}</h2>
+        <a href={props.link} target="_blank" rel="noreferrer">
+        <button>Watch now</button></a>
     </div>
+
+</div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App.js';
 
 ReactDOM.render(
     <>
+
+<div className="parent">
     <App   imgsrc="./mg.jpg"
         info="South Indian Movie in Hindi"
         sname="Maghadheera"
@@ -19,11 +21,25 @@ ReactDOM.render(
         sname="Maghadheera"
         link="https://www.youtube.com/watch?v=js4HJnv4Zu4" 
     />
+    </div>
+    <div className="parent">
+    <App   imgsrc="./mg.jpg"
+        info="South Indian Movie in Hindi"
+        sname="Maghadheera"
+        link="https://www.youtube.com/watch?v=js4HJnv4Zu4" 
+    />
     <App   imgsrc="./phantom.jpg"
         info="Bollywood movies"
         sname="Phantom"
         link="https://www.youtube.com/watch?v=U7vBj2AGG3c" 
     />
+    <App   imgsrc="./mg.jpg"
+        info="South Indian Movie in Hindi"
+        sname="Maghadheera"
+        link="https://www.youtube.com/watch?v=js4HJnv4Zu4" 
+    />
+    </div>
+    
     </> ,
     document.getElementById("root")
 );
